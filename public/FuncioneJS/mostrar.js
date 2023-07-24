31ec5eb761d0004ee6913e0eb1a5da4ecc075da4
Felipe Correa <felipeco0322@gmail.com>
2023-07-23 22:00:13 -0500

    Crea las siguientes 4 preguntas (5-8). Crea condiciones de las preguntas.

--- public/FuncioneJS/mostrar.js
+++ public/FuncioneJS/mostrar.js
@@ -10,3 +10,15 @@ function mosP4(){
   window.location.href = "#P4";
 }
 
+function mosP6(){
+  window.location.href = "#P6";
+}
+
+function mosP7(){
+  window.location.href = "#P7";
+}
+
+function mosP8(){
+  window.location.href = "#P8";
+}
+
